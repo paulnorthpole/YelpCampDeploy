64c6d5e4f4b281269c15d350e8a5b52faf3d49ff
Paul Charles <PCharles71@RocketMail.com>
2017-12-01 22:47:55 -0900

    add mongolab

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v8");
+//mongoose.connect("mongodb://localhost/yelp_camp_v8");
+const databaseUrl = "mongodb://paul:Skyler11@ds127936.mlab.com:27936/yelpcamppaul";
+mongoose.connect(databaseUrl, {useMongoClient: true}).then(() => console.log("Database connected")).catch(err => console.log("Database connection error: ${err.message}"));
+
 mongoose.Promise = global.Promise;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -41,8 +45,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
+  res.locals.error       = req.flash("error");
+  res.locals.success     = req.flash("success");
   next();
 });
 
