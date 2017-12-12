c530e6f9d54e9d33b86acc129e269bfb34789ea3
Paul Charles <pcharles71@rocketmail.com>
2017-12-12 13:41:18 -0900

    add password reset option and auto email.

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ to update live site...
     1. push changes to github.
     2. when ready to deploy..
         a. git push heroku master
+
+For hiding passwords and tokens
+
+    1. install dotenv from node
+        a. npm i -S dotenv
+        b. require it on page
+            a. dotenv = require("dotenv").config()
+            b. create a .env file in root
+            c. add key value pairs to file
+        c. use process.env.KEY where you want to keep secret
+            a. dont add .env file to version control
+
+    2. export <name>=<secret>
+       export GMAILPW=ourPassword in the terminal
+       this will allow you to access the key with  process.env.GMAILPW
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-var express        = require("express"),
-    router         = express.Router(),
-    passport       = require("passport"),
-    User           = require("../models/user"),
-    Campground     = require("../models/campground");
+var express    = require("express"),
+    router     = express.Router(),
+    passport   = require("passport"),
+    User       = require("../models/user"),
+    Campground = require("../models/campground"),
+    async      = require("async"),
+    nodemailer = require("nodemailer"),
+    dotenv = require("dotenv").config(),
+    crypto     = require("crypto");
 
 // Root Route
 router.get("/", function(req, res) {
@@ -59,6 +63,133 @@ router.get("/logout", function(req, res) {
 	res.redirect("/campgrounds");
 });
 
+
+// forgotten password reset
+
+router.get('/forgot', function(req, res) {
+  res.render('forgot');
+});
+
+router.post('/forgot', function(req, res, next) {
+	async.waterfall([
+		function(done) {
+			crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+		function(token, done) {
+			User.findOne({ email: req.body.email }, function(err, user) {
+				if (!user) {
+				    req.flash('error', 'No account with that email address exists.');
+				    return res.redirect('/forgot');
+				}
+
+				user.resetPasswordToken = token;
+				user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+				user.save(function(err) {
+					done(err, token, user);
+        });
+      });
+    },
+		function(token, user, done) {
+		var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.GMAIL,
+          pass: process.env.GMAILPW
+        }
+      });
+		var mailOptions = {
+			to: user.email,
+			from: process.env.GMAIL,
+			subject: 'Node.js Password Reset',
+			text: 'You are receiving this E-Mail because you (or someone else) has requested the reset' +
+			      'of your password.  Please click on the following link, or paste this into your browser' +
+			      'to complete the process.  ' +
+			      'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+						'If you did not request this, please ignore this email and your password will remain unchanged'
+		};
+		smtpTransport.sendMail(mailOptions, function(err) {
+      console.log('mail sent');
+      req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+      done(err, 'done');
+		});
+	}
+], function(err) {
+		if (err) return next(err);
+		res.redirect('/forgot');
+  })
+});
+
+
+// get and ask to reset password
+
+router.get('/reset/:token', function(req, res) {
+	User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+		if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/forgot');
+		}
+		res.render('reset', {token: req.params.token});
+  });
+});
+
+
+// This is where we enter the new password and confirm it.
+
+router.post('/reset/:token', function(req, res) {
+	async.waterfall([
+		function(done) {
+			User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+				if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+				}
+				if(req.body.password === req.body.confirm) {
+					user.setPassword(req.body.password, function(err) {
+						user.resetPasswordToken = undefined;
+						user.resetPasswordExpires = undefined;
+
+						user.save(function(err) {
+							req.logIn(user, function(err) {
+								done(err, user);
+              });
+            });
+          })
+				} else {
+          req.flash('error', "Passwords do not match.");
+          return res.redirect('back');
+				}
+      });
+    },
+		function(user, done) {
+			var smtpTransport = nodemailer.createTransport({
+				service: "Gmail",
+				auth: {
+					user: process.env.GMAIL,
+					pass: process.env.GMAILPW
+				}
+			});
+			var mailOptions = {
+				to: user.email,
+				from: process.env.GMAIL,
+				subject: 'Your password has been changed',
+				text: 'Hello, \n\n' +
+					'This is a confirmation that the password for your account ' + user.email + ' has just be changed.'
+			};
+			smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+	], function(err) {
+    res.redirect('/campgrounds');
+  });
+});
+
+
 // User Profile
 router.get("/users/:id", function(req, res) {
   User.findById(req.params.id, function(err, foundUser) {
@@ -71,10 +202,14 @@ router.get("/users/:id", function(req, res) {
         req.flash("error", "Something went wrong.");
         res.redirect("/");
       }
-    	res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+      res.render("users/show", {user: foundUser, campgrounds: campgrounds});
 
     })
   });
 });
 
+
 module.exports = router;
+
+
+
