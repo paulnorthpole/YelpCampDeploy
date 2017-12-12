829c3ed82d405bd299bd43f734dc116b4c4d2070
Paul Charles <PCharles71@RocketMail.com>
2017-12-11 17:34:05 -0900

    Create user profiles

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express  = require("express"),
-    router   = express.Router(),
-    passport = require("passport"),
-    User     = require("../models/user");
+var express        = require("express"),
+    router         = express.Router(),
+    passport       = require("passport"),
+    User           = require("../models/user"),
+    Campground     = require("../models/campground");
 
 // Root Route
 router.get("/", function(req, res) {
@@ -15,7 +16,13 @@ router.get("/register", function(req, res) {
 
 //handle sign up logic
 router.post("/register", function(req, res) {
-	var newUser = new User({username: req.body.username});
+	var newUser = new User({
+      username:  req.body.username,
+      firstName: req.body.firstName,
+      lastName:  req.body.lastName,
+      email:     req.body.email,
+      avatar:    req.body.avatar
+		});
 	if (req.body.adminCode === "secretcode123") {
 	    newUser.isAdmin = true;
 	}
@@ -50,6 +57,24 @@ router.get("/logout", function(req, res) {
 	req.logout();
   req.flash("success", "Logged you out!");
 	res.redirect("/campgrounds");
+});
+
+// User Profile
+router.get("/users/:id", function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    if (err) {
+      req.flash("error", "Something went wrong.");
+      res.redirect("/");
+    }
+    Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds) {
+      if (err) {
+        req.flash("error", "Something went wrong.");
+        res.redirect("/");
+      }
+    	res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+
+    })
+  });
 });
 
 module.exports = router;
